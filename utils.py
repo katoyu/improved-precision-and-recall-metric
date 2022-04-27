59e2ce63c5c111afdf92aa5cd844d9412e3d6154
katoyu <yuik2525@gmail.com>
2022-04-27 05:40:27 +0000

    modify link of pkl

--- utils.py
+++ utils.py
@@ -30,7 +30,9 @@ def init_tf(random_seed=1234):
 def initialize_stylegan():
     """Load StyleGAN network pickle."""
     print('Initializing StyleGAN...\n')
-    url = 'https://drive.google.com/uc?id=1zwhFLIvQYyNVOwQCLAhIJHmN7sjvAM84' # karras2019stylegan-ffhq-1024x1024.pkl
+    # url = 'https://drive.google.com/uc?id=1zwhFLIvQYyNVOwQCLAhIJHmN7sjvAM84' # karras2019stylegan-ffhq-1024x1024.pkl    
+    url = 'https://drive.google.com/uc?id=1zwhFLIvQYyNVOwQCLAhIJHmN7sjvAM84&confirm=t' # karras2019stylegan-ffhq-1024x1024.pkl
+    
     with dnnlib.util.open_url(url, cache_dir=os.path.join(BASE_PATH, '_cache')) as f:
         _, _, Gs = pickle.load(f) 
     return Gs
@@ -40,7 +42,9 @@ def initialize_stylegan():
 def initialize_feature_extractor():
     """Load VGG-16 network pickle (returns features from FC layer with shape=(n, 4096)).""" 
     print('Initializing VGG-16 model...')
-    url = 'https://drive.google.com/uc?id=1fk6r8vetqpRShtEODXm9maDytbMkHLfa' # vgg16.pkl
+    # url = 'https://drive.google.com/uc?id=1fk6r8vetqpRShtEODXm9maDytbMkHLfa' # vgg16.pkl
+    url = 'https://drive.google.com/uc?id=1fk6r8vetqpRShtEODXm9maDytbMkHLfa&confirm=t' # vgg16.pkl
+    
     with dnnlib.util.open_url(url, cache_dir=os.path.join(BASE_PATH, '_cache')) as f:
         _, _ , net = pickle.load(f)
     return net
